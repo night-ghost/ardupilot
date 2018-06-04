fcdead08ffdf23cb3081053402e9d576ad4f9856
night-ghost <night_ghost@ykoctpa.ru>
2018-06-04 10:28:03 +0500

    fixed some boards descriptions

--- libraries/AP_HAL_F4Light/boards/f4light_MatekF405_CTR/board.h
+++ libraries/AP_HAL_F4Light/boards/f4light_MatekF405_CTR/board.h
@@ -168,11 +168,6 @@
 # define HAL_BATT_VOLT_SCALE    10.1
 # define HAL_BATT_CURR_SCALE    17
 
-# define HAL_BATT_VOLT_PIN      PC5 // ChibiOS compatible defines
-# define HAL_BATT_CURR_PIN      PC4
-# define HAL_BATT_VOLT_SCALE    10.1
-# define HAL_BATT_CURR_SCALE    17
-
 #define BOARD_USB_DMINUS 108
 
 //#define BOARD_SBUS_INVERTER     
